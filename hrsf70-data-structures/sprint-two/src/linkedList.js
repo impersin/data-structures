4a3e3d050e230d1f7084e18fccdf8f137945175b
impersin <closer124@gmail.com>
2016-12-24 18:20:26 -0800

    Completed pesudoclassical instantiation with notaions.

--- hrsf70-data-structures/sprint-two/src/linkedList.js
+++ hrsf70-data-structures/sprint-two/src/linkedList.js
@@ -1,36 +1,57 @@
 var LinkedList = function() {
-  var list = {};
-  list.head = null;
-  list.tail = null;
+  this.head = null;
+  this.tail = null;
+};
 
-  list.addToTail = function(value) {
-    var node = Node(value);
-      if(!list.head){
-        this.head = node; 
-      }else{
-        this.tail.next = node;
-      }
-        this.tail = node;
-    };
-
-  list.removeHead = function() {
-      var temp = this.head.value;
-      this.head=this.head.next;
-        return temp;
+  LinkedList.prototype.addToTail = function(value) {
+    // create a new tail.
+    var newTail = new Node(value);
+    // if there is no head.
+    if(!this.head){
+      //first newTail will be head & tail.
+      this.head = newTail;
+      //if there is a head, it means there is a tail already.
+    }else{
+      //newTail will be a new tail.
+      //do not get confuse head and tail are reference of newTail instance!!!.
+      this.tail.next = newTail; // == > this.head.next = newTail as well.!!
+    }
+    //new tail always will be assign to tail property.
+    this.tail = newTail;
   };
 
-  list.contains = function(target) {
-      var start = this.head;
-      while(start){
-        if(start.value === target){
+  LinkedList.prototype.removeHead = function() {
+     // store value before remove head.
+      var temp = this.head.value;
+      // next instance will be new head.
+      this.head = this.head.next;
+      //return removed head value.
+      return temp;
+  };
+
+  LinkedList.prototype.contains = function(target) {
+    var head = this.head;
+    
+    //create recoursion function.
+      function traverse(node) {
+        //check if first head value === target.
+        if(node.value === target){
+          //if it is return true'
           return true;
         }
-        start = start.next;
+        //if it is not check if there is tail object.
+        if(node.next !== null){
+          // if there is pass tail object as argument to recourse.
+          return traverse(node.next);
+        }else{
+          //if there is no tail that mean there is value same as target.
+          return false;
+        }
       }
-      return false;
+      //recoursion starts.
+      return traverse(head)
   };
-  return list;
-};
+
 
 var Node = function(value) {
   var node = {};
@@ -40,6 +61,49 @@ var Node = function(value) {
 
   return node;
 };
+
+// var LinkedList = function() {
+//   var list = {};
+//   list.head = null;
+//   list.tail = null;
+
+//   list.addToTail = function(value) {
+//     var node = Node(value);
+//       if(!list.head){
+//         this.head = node; 
+//       }else{
+//         this.tail.next = node;
+//       }
+//         this.tail = node;
+//     };
+
+//   list.removeHead = function() {
+//       var temp = this.head.value;
+//       this.head=this.head.next;
+//         return temp;
+//   };
+
+//   list.contains = function(target) {
+//       var start = this.head;
+//       while(start){
+//         if(start.value === target){
+//           return true;
+//         }
+//         start = start.next;
+//       }
+//       return false;
+//   };
+//   return list;
+// };
+
+// var Node = function(value) {
+//   var node = {};
+
+//   node.value = value;
+//   node.next = null;
+
+//   return node;
+// };
 
 /*
  * Complexity: What is the time complexity of the above functions?
