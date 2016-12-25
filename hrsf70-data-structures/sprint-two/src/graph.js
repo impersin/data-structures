f739710a4c4b07a96855ccc3276f62268489e741
impersin <closer124@gmail.com>
2016-12-25 00:08:49 -0800

    Implemete removeNode

--- hrsf70-data-structures/sprint-two/src/graph.js
+++ hrsf70-data-structures/sprint-two/src/graph.js
@@ -1,29 +1,47 @@
 // Instantiate a new graph
 var Graph = function() {
+
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this[node] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  if(this[node]){
+    return true;
+  }else{
+    return false;
+  }
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+    for(var key in this){
+      
+    }  
+  delete this[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return this[fromNode].indexOf(toNode) > -1 && this[toNode].indexOf(fromNode) > -1
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this[fromNode].push(toNode);
+  this[toNode].push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  var toNodeIdx = this[fromNode].indexOf(toNode);
+  var fromNodeIdx = this[toNode].indexOf(fromNode);
+  this[fromNode].splice(toNodeIdx, 1);
+  this[toNode].splice(fromNodeIdx, 1);
 };
 
 // Pass in a callback which will be executed on each node of the graph.
