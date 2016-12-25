40f3a92e1f15eff6f3953cefc5208cbca47f9681
impersin <closer124@gmail.com>
2016-12-24 19:43:05 -0800

    Undo all the implementation in order to redo.

--- hrsf70-data-structures/sprint-two/src/tree.js
+++ hrsf70-data-structures/sprint-two/src/tree.js
@@ -1,10 +1,10 @@
+
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-  // your code here
-  newTree.children = [];  // fix me
 
-  _.extend(newTree, treeMethods)
+  // your code here
+  newTree.children = null;  // fix me
 
   return newTree;
 };
@@ -12,28 +12,7 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-    this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
-  var children = this; //array
-  var bool = false;
-  function inner(child){
-    if(child.value === target){
-      bool = true;
-    }else if(child.children){
-      for(var i = 0; i< child.children.length;i++){
-        inner(child.children[i]);
-      }
-    }
-  }
-  inner(children);
-  return bool;
 };
-
-
-// var tree = newTree 
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
